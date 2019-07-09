133e30b629aacca0db45974deadcb6a78000d534
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-09 14:24:42 -0500

    Added basic properties to Level poco

--- FPT.Core/Model/Level.cs
+++ FPT.Core/Model/Level.cs
@@ -6,5 +6,10 @@ namespace FPT.Core.Model
 {
     public class Level
     {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string InitializerFilepath { get; set; }
+        public string VerifierFilepath { get; set; }
+        public string InstructionsFilepath { get; set; }
     }
 }
