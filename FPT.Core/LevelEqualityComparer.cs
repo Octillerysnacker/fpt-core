c2af679cec46b947066ebfa55dcbe478f56cdf90
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-12 19:07:54 +0900

    Added shoddily implemented LevelEqualityComparer

---
+++ FPT.Core/LevelEqualityComparer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FPT.Core.Model;
+namespace FPT.Core
+{
+    //TO-DO: Veriy equality comparer is correctly implemented
+    public class LevelEqualityComparer : IEqualityComparer<Level>
+    {
+        public bool Equals(Level x, Level y)
+        {
+            return x.Id == y.Id &&
+                x.Name == y.Name &&
+                x.InitializerFilepath == y.InitializerFilepath &&
+                x.VerifierFilepath == y.VerifierFilepath &&
+                x.InstructionsFilepath == y.InstructionsFilepath &&
+                x.FolderFilepath == y.FolderFilepath;
+        }
+
+        public int GetHashCode(Level obj)
+        {
+            unchecked
+            {
+                return obj.Name.GetHashCode() * 3 +
+                    obj.Id.GetHashCode() * 5 +
+                    obj.InitializerFilepath.GetHashCode() * 7 +
+                    obj.VerifierFilepath.GetHashCode() * 11 +
+                    obj.InstructionsFilepath.GetHashCode() * 13 +
+                    obj.FolderFilepath.GetHashCode() * 17;
+            }
+        }
+    }
+}
