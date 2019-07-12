11856a8ae5207fbc4e0c9e40c2750c069e18eeb8
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-12 22:10:07 +0900

    Added a separate ConsoleApp project

---
+++ FPT.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FPT.Core.Commands;
+using FPT.Core.IO;
+using System.IO.Abstractions;
+namespace FPT.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mainCommand = new RouterCommand();
+            mainCommand.Register(new GetLevelsCommand("levels", new FileSystemLevelsProvider(new FileSystem(), "")));
+
+            mainCommand.Execute(args);
+        }
+    }
+}
