4ec2de57d512f205afdfd1954107ae18f25c44d3
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-13 14:12:16 +0800

    Added basic ReturnCommand

---
+++ FPT.Core.Tests/Commands/ReturnCommand.cs
@@ -0,0 +1,20 @@
+﻿using FPT.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPT.Core.Commands
+{
+    public class ReturnCommand : Command
+    {
+        private readonly object objToReturn;
+        public ReturnCommand(string commandId, object objToReturn) : base(commandId)
+        {
+            this.objToReturn = objToReturn;
+        }
+        public override object Execute(params string[] args)
+        {
+            return objToReturn;
+        }
+    }
+}
