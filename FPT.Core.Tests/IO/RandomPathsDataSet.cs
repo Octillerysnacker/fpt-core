1097cc463504e0f67d3c77a8207ef8da6b345500
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-18 23:57:09 +0800

    Made RandomPathsDataSet its own class

---
+++ FPT.Core.Tests/IO/RandomPathsDataSet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace FPT.Core.Tests.IO
+{
+    public class RandomPathsDataSet : TheoryData<string>
+    {
+        private int numberOfDataSets = 4;
+        public RandomPathsDataSet()
+        {
+            for (int i = 0; i < numberOfDataSets; i++)
+            {
+                Add(Path.Combine("c:", Path.GetRandomFileName()));
+            }
+        }
+    }
+}
