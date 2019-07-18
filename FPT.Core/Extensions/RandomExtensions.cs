528f859b1956f985385e16a65779072f4d8710c0
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-18 15:33:19 +0800

    Added RandomExtensions class

---
+++ FPT.Core/Extensions/RandomExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPT.Core.Extensions
+{
+    public static class RandomExtensions
+    {
+        private static readonly string alphanumericCharacters = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+        public static string RandomString(this Random random, int length)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0; i < length; i++)
+            {
+                stringBuilder.Append(alphanumericCharacters[random.Next(alphanumericCharacters.Length)]);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
