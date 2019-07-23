3de0861ef1f904e38c23ceff8e7f8d2ab633abe4
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-23 19:00:33 +0800

    Created proper tests for OpenLevelCommand

--- FPT.Core.Tests/Commands/OpenLevelCommand_ExecuteShould.cs
+++ FPT.Core.Tests/Commands/OpenLevelCommand_ExecuteShould.cs
@@ -7,22 +7,68 @@ using FPT.Core.IO;
 using FPT.Core.Tests.IO;
 using FPT.Core.Model;
 using Xunit;
-
+using FPT.Core.Tests.Extensions;
+using FPT.Core.Extensions;
+using System.IO;
 namespace FPT.Core.Tests.Commands
 {
     public class OpenLevelCommand_ExecuteShould
     {
-        [Fact(Skip = "Testing will result in identical test and production code")]
-        public void ReturnProjectFolderPathForSpecifiedLevelAndUser()
+        [Theory]
+        [ClassData(typeof(ProjectFolderTestData))]
+        public void ReturnProjectFolderPathForSpecifiedLevelAndUser(FakeLevelsProvider provider, string levelId, string user, string expectedPath)
         {
+            var command = new OpenLevelCommand("", new MockLevelInitializer(), provider);
+
+            var result = command.Execute(levelId, user);
+
+            Assert.Equal(expectedPath, result);
         }
-        [Fact(Skip = "All this tests is an if statement")]
-        public void RunInitializerWhenNecessary()
+        [Theory]
+        [ClassData(typeof(CorrectParametersTestData))]
+        public void SupplyCorrectParametersToInitializer(FakeLevelsProvider provider, string levelId, string user)
         {
+            var initializer = new MockLevelInitializer();
+            var command = new OpenLevelCommand("", initializer, provider);
+
+            command.Execute(levelId, user);
+
+            Assert.Equal(levelId, initializer.SuppliedLevelId);
+            Assert.Equal(user, initializer.SuppliedUser);
         }
-        [Fact(Skip = "All this tests is an if statement")]
-        public void DoNotRunInitializerWhenNotNecessary()
+        public class ProjectFolderTestData : TheoryData<FakeLevelsProvider, string, string, string>
         {
+            private int dataSetCount = 4;
+            private RandomTriadFactory factory = new RandomTriadFactory();
+            public ProjectFolderTestData()
+            {
+                for(int i = 0; i < dataSetCount; i++)
+                {
+                    CreateAndAddRandomizedData();
+                }
+            }
+            private void CreateAndAddRandomizedData()
+            {
+                var triad = factory.CreateTriad();
+
+                var expectedPath = Path.Combine(triad.Level.FolderFilepath, triad.User, "project");
+                Add(triad.Provider, triad.Level.Id, triad.User, expectedPath);
+            }
         }
+        public class CorrectParametersTestData : TheoryData<FakeLevelsProvider,string, string>
+        {
+            private int dataSetCount = 4;
+            private RandomTriadFactory factory = new RandomTriadFactory();
+            public CorrectParametersTestData()
+            {
+                for(int i = 0; i< dataSetCount; i++)
+                {
+                    var triad = factory.CreateTriad();
+
+                    Add(triad.Provider, triad.Level.Id, triad.User);
+                }
+            }
+        }
+
     }
 }
