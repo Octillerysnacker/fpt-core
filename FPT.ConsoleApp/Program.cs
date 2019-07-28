f5ae9be07575ddcb3b8cc066380232fff6bf6e65
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-28 23:16:27 +0800

    Revert "Removed ConsoleApp from repo"
    This reverts commit 8103fc49154ecc21a20b8e578d1109e0ccbcc5c8.

---
+++ FPT.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FPT.Core.Commands;
+using FPT.Core.IO;
+using System.IO.Abstractions;
+namespace FPT.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object output = "Nothing was serialized.";
+            try
+            {
+                var mainCommand = new RouterCommand();
+                var fileSystem = new FileSystem();
+                var levelsProvider = new FileSystemLevelsProvider(fileSystem, ".");
+                mainCommand.Register(new GetLevelsCommand("levels", levelsProvider));
+                mainCommand.Register(new OpenLevelCommand("open", new MasterFolderLevelInitializer(new UserJsonLevelInitializationDeterminer(fileSystem), new CopyDir(fileSystem), levelsProvider,fileSystem.Path),levelsProvider));
+                output = mainCommand.Execute(args);
+            }
+            catch (Exception e)
+            {
+                output = e;
+            }
+            finally
+            {
+                Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(output));
+            }
+        }
+    }
+}
