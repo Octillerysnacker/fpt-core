149ab7e10d2c4fe00d9265b6940fa06011335516
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-31 20:35:18 +0800

    Added ThrowerCommand

---
+++ FPT.Core.Tests/Commands/ThrowerCommand.cs
@@ -0,0 +1,20 @@
+﻿using FPT.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPT.Core.Tests.Commands
+{
+    public class ThrowerCommand : IExecutable
+    {
+        Exception e;
+        public ThrowerCommand(Exception e)
+        {
+            this.e = e;
+        }
+        public object Execute(params string[] args)
+        {
+            throw e;
+        }
+    }
+}
