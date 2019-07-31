90d4efbbfa8de9e4a56c888c2344c301e9644d7c
Octillerysnacker <octillerysnacker@gmail.com>
2019-07-31 20:34:57 +0800

    Added InvalidCommandArrayException

---
+++ FPT.Core/Exceptions/InvalidCommandArrayException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPT.Core.Exceptions
+{
+    public class InvalidCommandArrayException : Exception
+    {
+        public InvalidCommandArrayException(string message) : base(message)
+        {
+        }
+
+        public InvalidCommandArrayException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
