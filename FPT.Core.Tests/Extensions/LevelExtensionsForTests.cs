9d3f0ab5b8f4916073ed8ac23e26e2fe0d0a21c3
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-05 05:15:29 +0800

    Added GenerateRandomLevel to LevelExtensionsForTests

--- FPT.Core.Tests/Extensions/LevelExtensionsForTests.cs
+++ FPT.Core.Tests/Extensions/LevelExtensionsForTests.cs
@@ -9,19 +9,23 @@ namespace FPT.Core.Tests.Extensions
     public static class LevelExtensionsForTests
     {
         public static int LengthOfRandomStrings { get; set; } = 25;
+        
         public static FakeLevelsProvider GenerateRandomLevelsWithProvider(this Random random, int minLevels, int maxLevels)
         {
             var amountOfLevels = random.Next(minLevels, maxLevels);
             var levels = new List<Level>();
 
-            string _() => random.RandomString(LengthOfRandomStrings);
-
             for (int i = 0; i < amountOfLevels; i++)
             {
-                levels.Add(new Level(_(),_(),_(),_(),_(),_()));
+                levels.Add(random.GenerateRandomLevel());
             }
             var levelsProvider = new FakeLevelsProvider(levels);
             return levelsProvider;
+        }
+        public static Level GenerateRandomLevel(this Random random)
+        {
+            string _() => random.RandomString(LengthOfRandomStrings);
+            return new Level(_(), _(), _(), _(), _(), _());
         }
     }
 }
