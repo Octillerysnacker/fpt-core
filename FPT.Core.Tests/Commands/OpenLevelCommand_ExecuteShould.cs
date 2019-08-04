6a2a68fa4320b2e4c9839e0fb04b6504ea42855e
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-05 00:38:34 +0800

    Added some error handling to OpenLevelCommand

--- FPT.Core.Tests/Commands/OpenLevelCommand_ExecuteShould.cs
+++ FPT.Core.Tests/Commands/OpenLevelCommand_ExecuteShould.cs
@@ -10,6 +10,8 @@ using Xunit;
 using FPT.Core.Tests.Extensions;
 using FPT.Core.Extensions;
 using System.IO;
+using FPT.Core.Exceptions;
+
 namespace FPT.Core.Tests.Commands
 {
     public class OpenLevelCommand_ExecuteShould
@@ -24,6 +26,25 @@ namespace FPT.Core.Tests.Commands
 
             Assert.Equal(expectedPath, result);
         }
+        public class ProjectFolderTestData : TheoryData<FakeLevelsProvider, string, string, string>
+        {
+            private int dataSetCount = 4;
+            private RandomTriadFactory factory = new RandomTriadFactory();
+            public ProjectFolderTestData()
+            {
+                for (int i = 0; i < dataSetCount; i++)
+                {
+                    CreateAndAddRandomizedData();
+                }
+            }
+            private void CreateAndAddRandomizedData()
+            {
+                var triad = factory.CreateTriad();
+
+                var expectedPath = Path.Combine(triad.Level.FolderFilepath, triad.User, "project");
+                Add(triad.Provider, triad.Level.Id, triad.User, expectedPath);
+            }
+        }
         [Theory]
         [ClassData(typeof(CorrectParametersTestData))]
         public void SupplyCorrectParametersToInitializer(FakeLevelsProvider provider, string levelId, string user)
@@ -36,23 +57,25 @@ namespace FPT.Core.Tests.Commands
             Assert.Equal(levelId, initializer.SuppliedLevelId);
             Assert.Equal(user, initializer.SuppliedUser);
         }
-        public class ProjectFolderTestData : TheoryData<FakeLevelsProvider, string, string, string>
+        [Theory]
+        [ClassData(typeof(TooFewParametersTestData))]
+        public void ThrowWhenTooFewParametersPassed(string[] args, string errorMessage)
         {
-            private int dataSetCount = 4;
-            private RandomTriadFactory factory = new RandomTriadFactory();
-            public ProjectFolderTestData()
-            {
-                for(int i = 0; i < dataSetCount; i++)
-                {
-                    CreateAndAddRandomizedData();
-                }
-            }
-            private void CreateAndAddRandomizedData()
-            {
-                var triad = factory.CreateTriad();
+            var command = new OpenLevelCommand(new MockLevelInitializer(), new FakeLevelsProvider(new List<Level>()));
 
-                var expectedPath = Path.Combine(triad.Level.FolderFilepath, triad.User, "project");
-                Add(triad.Provider, triad.Level.Id, triad.User, expectedPath);
+            var result = Assert.Throws<InvalidCommandArrayException>(() => command.Execute(args));
+            Assert.Equal(errorMessage, result.Message);
+        }
+        private class TooFewParametersTestData : TheoryData<string[], string>
+        {
+            private Random random = new Random();
+            private int arbritraryLength = 25;
+            public TooFewParametersTestData()
+            {
+                var errorMessage = "Not enough parameters were passed ({0} out of 2).";
+                Add(null, string.Format(errorMessage, 0));
+                Add(new string[] { }, string.Format(errorMessage, 0));
+                Add(new string[] { random.RandomString(arbritraryLength) }, string.Format(errorMessage, 1));
             }
         }
         public class CorrectParametersTestData : TheoryData<FakeLevelsProvider,string, string>
--- FPT.Core/Commands/OpenLevelCommand.cs
+++ FPT.Core/Commands/OpenLevelCommand.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using FPT.Core.IO;
 using System.Linq;
+using FPT.Core.Exceptions;
+
 namespace FPT.Core.Commands
 {
     public class OpenLevelCommand : IExecutable
     {
         private ILevelInitializer levelInitializer;
         private ILevelsProvider levelsProvider;
+        private string errorMessageFormat = "Not enough parameters were passed ({0} out of 2).";
         public OpenLevelCommand(ILevelInitializer levelInitializer, ILevelsProvider levelsProvider)
         {
             this.levelInitializer = levelInitializer;
@@ -17,6 +20,13 @@ namespace FPT.Core.Commands
 
         public object Execute(params string[] args)
         {
+            if(args is null)
+            {
+                throw new InvalidCommandArrayException(string.Format(errorMessageFormat, 0));
+            }else if(args.Length < 2)
+            {
+                throw new InvalidCommandArrayException(string.Format(errorMessageFormat, args.Length));
+            }
             string levelId = args[0];
             string user = args[1];
 
