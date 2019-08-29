11fe33705a77c40f7aec487e4238dcf0e2005a79
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-29 16:47:55 -0500

    Added VerifyCommand with tests

---
+++ FPT.Core.Tests/Commands/VerifyCommand_ExecuteShould.cs
@@ -0,0 +1,81 @@
+﻿using FPT.Core.Commands;
+using FPT.Core.Exceptions;
+using FPT.Core.Extensions;
+using FPT.Core.Tests.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FPT.Core.Tests.Commands
+{
+    public class VerifyCommand_ExecuteShould
+    {
+        [Theory]
+        [ClassData(typeof(RunCorrectVerifierForUser_Data))]
+        public void RunCorrectVerifierForUser(FakeLevelsProvider provider, string levelId, string user)
+        {
+            var mockVerifier = new MockVerifier();
+            var command = new VerifyCommand(provider,mockVerifier);
+
+            command.Execute(levelId, user);
+
+            Assert.Equal(levelId, mockVerifier.Level.Id);
+            Assert.Equal(user, mockVerifier.User);
+        }
+        private class RunCorrectVerifierForUser_Data : TheoryData<FakeLevelsProvider, string, string>
+        {
+            private int datasetCount = 4;
+            private RandomTriadFactory factory = new RandomTriadFactory();
+            public RunCorrectVerifierForUser_Data()
+            {
+                for(int i = 0; i < datasetCount; i++)
+                {
+                    var triad = factory.CreateTriad();
+                    Add(triad.Provider, triad.Level.Id, triad.User);
+                }
+            }
+        }
+        [Theory]
+        [InlineData(0)]
+        [InlineData("  ")]
+        [InlineData(false)]
+        [InlineData(null)]
+        [InlineData(3.14)]
+        public void ReturnResultFromVerifier(object toReturn)
+        {
+            var verifier = new MockVerifier() { Return = toReturn };
+            var provider = new CustomFakeLevelsProvider() { Level = null };
+            var command = new VerifyCommand(provider, verifier);
+
+            var result = command.Execute("", "");
+
+            Assert.Equal(toReturn, result);
+        }
+        [Theory]
+        [ClassData(typeof(ThrowWhenTooFewParametersPassed_Data))]
+        public void ThrowWhenTooFewParametersPassed(string[] args, string expected)
+        {
+            var verifier = new MockVerifier();
+            var provider = new CustomFakeLevelsProvider();
+            var command = new VerifyCommand(provider, verifier);
+
+            var e = Assert.Throws<InvalidCommandArrayException>(() => command.Execute(args));
+            Assert.Equal(expected, e.Message);
+        }
+        private class ThrowWhenTooFewParametersPassed_Data : TheoryData<string[], string>
+        {
+            private Random random = new Random();
+            public ThrowWhenTooFewParametersPassed_Data()
+            {
+                Add(null, FormatMessage(0));
+                Add(new string[] { }, FormatMessage(0));
+                Add(new string[] { random.RandomString(10) }, FormatMessage(1));
+            }
+            private string FormatMessage(int missingParamCount)
+            {
+                return $"Not enough parameters were passed ({missingParamCount} out of 2).";
+            }
+        }
+    }
+}
---
+++ FPT.Core/Commands/VerifyCommand.cs
@@ -0,0 +1,36 @@
+﻿using FPT.Core.Exceptions;
+using FPT.Core.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPT.Core.Commands
+{
+    public class VerifyCommand : IExecutable
+    {
+        private ILevelsProvider provider;
+        private IVerifier verifier;
+        public VerifyCommand(ILevelsProvider provider, IVerifier verifier)
+        {
+            this.provider = provider;
+            this.verifier = verifier;
+        }
+        public object Execute(params string[] args)
+        {
+            if(args is null)
+            {
+                ThrowNotEnoughParams(0);
+            }else if(args.Length < 2){
+                ThrowNotEnoughParams(args.Length);
+            }
+            var levelId = args[0];
+            var user = args[1];
+            var level = provider.GetLevel(levelId);
+            return verifier.Verify(level, user);
+        }
+        private void ThrowNotEnoughParams(int missingParamCount)
+        {
+            throw new InvalidCommandArrayException($"Not enough parameters were passed ({missingParamCount} out of 2).");
+        }
+    }
+}
