349589c60c13f579b936c02e39da94977a4b67ee
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-31 17:33:00 -0500

    Added FPTDiagnosticExtensions

---
+++ FPT.Core.Tests/Extensions/FPTDiagnosticExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FPT.Core.Extensions;
+using FPT.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPT.Core.Tests.Extensions
+{
+    public static class FPTDiagnosticExtensions
+    {
+        public static FPTDiagnostic NextFPTDiagnostic(this Random random)
+        {
+            return new FPTDiagnostic(
+                            random.RandomString(10),
+                            (FPTDiagnostic.DiagnosticKind)random.Next(0, 3),
+                            new FPTDiagnostic.FileLocation(random.Next(0, 10), random.Next(0, 10)),
+                            new FPTDiagnostic.FileLocation(random.Next(0, 10), random.Next(0, 10))
+                            );
+        }
+    }
+}
