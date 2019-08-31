8a324c2f327db8cde6500149b03309b1a3cfe6f9
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-31 17:33:50 -0500

    Added FPTDiagnostic test

---
+++ FPT.Core.Tests/FPTDiagnostic_Should.cs
@@ -0,0 +1,36 @@
+﻿using FPT.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Newtonsoft.Json;
+using FPT.Core.Tests.Extensions;
+
+namespace FPT.Core.Tests
+{
+    public class FPTDiagnostic_Should
+    {
+        [Theory]
+        [ClassData(typeof(BeSerializedAndDeserializedProperly_Data))]
+        public void BeSerializedAndDeserializedProperly(FPTDiagnostic diagnostic)
+        {
+            var serialized = JsonConvert.SerializeObject(diagnostic);
+
+            var deserialized = JsonConvert.DeserializeObject<FPTDiagnostic>(serialized);
+
+            Assert.Equal(diagnostic, deserialized, new FPTDiagnosticEqualityComparer());
+        }
+        private class BeSerializedAndDeserializedProperly_Data : TheoryData<FPTDiagnostic>
+        {
+            private int datasetCount = 4;
+            private Random random = new Random();
+            public BeSerializedAndDeserializedProperly_Data()
+            {
+                for(int i = 0; i < datasetCount; i++)
+                {
+                    Add(random.NextFPTDiagnostic());
+                }
+            }
+        }
+    }
+}
