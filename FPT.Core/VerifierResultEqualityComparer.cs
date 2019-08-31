9681a8d8c180cf2ade0299eb85bf3f33b4225082
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-31 17:34:52 -0500

    Added VerifierResultEqualityComparer

---
+++ FPT.Core/VerifierResultEqualityComparer.cs
@@ -0,0 +1,28 @@
+﻿using FPT.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FPT.Core
+{
+    public class VerifierResultEqualityComparer : IEqualityComparer<VerifierResult>
+    {
+        private FPTDiagnosticEqualityComparer fComparer = new FPTDiagnosticEqualityComparer();
+        public bool Equals(VerifierResult x, VerifierResult y)
+        {
+            return x.Success == y.Success &&
+                x.Diagnostics.SequenceEqual(y.Diagnostics, fComparer);
+        }
+
+        public int GetHashCode(VerifierResult obj)
+        {
+            unchecked {
+                int hash = 51;
+                hash = hash * 71 + obj.Diagnostics.GetHashCode();
+                hash = hash * 71 + obj.Success.GetHashCode();
+                return hash;
+            }
+        }
+    }
+}
