383829050d95146c1411280eca61e159d892cefd
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-31 17:34:27 -0500

    Added MockJarFileProcessFactory

---
+++ FPT.Core.Tests/IO/MockJarFileProcessFactory.cs
@@ -0,0 +1,22 @@
+﻿using FPT.Core.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPT.Core.Tests.IO
+{
+    public class MockJarFileProcessFactory : IJarFileProcessFactory
+    {
+        public ISimplifiedProcess Process { get; set; }
+        public string JarPath { get; private set; }
+        public string UserFolder { get; private set; }
+        public string ProjectFolder { get; private set; }
+        public ISimplifiedProcess CreateProcess(string jarPath, string userFolder, string projectFolder)
+        {
+            JarPath = jarPath;
+            UserFolder = userFolder;
+            ProjectFolder = projectFolder;
+            return Process;
+        }
+    }
+}
