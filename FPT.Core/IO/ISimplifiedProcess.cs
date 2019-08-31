95cede6e6b8151fabc4711a6c1beccbb5cb64f1a
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-31 17:33:19 -0500

    Added ISimplifiedProcess

---
+++ FPT.Core/IO/ISimplifiedProcess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FPT.Core.IO
+{
+    public interface ISimplifiedProcess
+    {
+        StreamReader StandardOutput { get; }
+    }
+}
