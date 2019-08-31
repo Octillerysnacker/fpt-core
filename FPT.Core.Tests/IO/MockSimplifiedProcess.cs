c3f01b6b3f29ae66c63be3e5a070c1ab3cefd2f2
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-31 17:34:11 -0500

    Added MockSimplifiedProcess

---
+++ FPT.Core.Tests/IO/MockSimplifiedProcess.cs
@@ -0,0 +1,19 @@
+﻿using FPT.Core.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FPT.Core.Tests.IO
+{
+    public class MockSimplifiedProcess : ISimplifiedProcess
+    {
+        public StreamReader StandardOutput { get; set; }
+        public bool HasBeenWaited { get; private set; }
+
+        public void WaitForExit()
+        {
+            HasBeenWaited = true;
+        }
+    }
+}
