59333f50ff4d1f2087314afbe996319b43b0278e
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-31 17:33:33 -0500

    Added IJarFileProcessFactory

---
+++ FPT.Core/IO/IJarFileProcessFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPT.Core.IO
+{
+    public interface IJarFileProcessFactory
+    {
+        ISimplifiedProcess CreateProcess(string jarPath, string userFolder, string projectFolder);
+    }
+}
