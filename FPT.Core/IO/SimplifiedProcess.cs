c839d2ed3f764584e0920e50b8dbe6d9e50e8ec2
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-01 01:35:11 -0500

    Changed SimplifiedProcess signature

---
+++ FPT.Core/IO/SimplifiedProcess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace FPT.Core.IO
+{
+    public class SimplifiedProcess : Process, ISimplifiedProcess
+    {
+    }
+}
