ad683f3b620417cd82eff9bd8601b820be2dcd88
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-03 20:35:36 -0500

    Added static path provider classes

---
+++ FPT.Core/IO/LevelPathsProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace FPT.Core.IO
+{
+    public static class LevelPathsProvider
+    {
+        public static string GetProjectFolder(string levelFolderPath,string user)
+        {
+            return Path.Combine(GetUserFolder(levelFolderPath,user), "project");
+        }
+        public static string GetUserFolder(string levelFolderPath, string user)
+        {
+            return Path.Combine(levelFolderPath, "users", user);
+        }
+    }
+}
---
+++ FPT.Core/IO/MasterFolderPathsProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace FPT.Core.IO
+{
+    public static class MasterFolderPathsProvider
+    {
+        public static string GetMasterFolder(string levelFolderFilepath)
+        {
+            return Path.Combine(levelFolderFilepath, "master");
+        }
+    }
+}
