406725b34db73a3d2ca08c61b067fdc542d7cd2e
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-03 19:15:18 -0500

    Added MainAppFactory

---
+++ FPT.Core/MainAppFactory.cs
@@ -0,0 +1,38 @@
+﻿using FPT.Core.Commands;
+using System.IO.Abstractions;
+using FPT.Core.Levels.Providers;
+using FPT.Core.Levels.Initialization;
+using FPT.Core.IO;
+using FPT.Core.Verification;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FPT.Core
+{
+    public class MainAppFactory
+    {
+        public static IExecutable MakeApp()
+        {
+            var main = new RouterCommand();
+            var fs = new FileSystem();
+            var provider = new FileSystemLevelsProvider(fs, ".");
+
+            main.Register("levels", new GetLevelsCommand(provider));
+            main.Register("open", new OpenLevelCommand(
+                new MasterFolderLevelInitializer(
+                    new UserJsonLevelInitializationDeterminer(fs),
+                    new CopyDir(fs),
+                    provider,
+                    fs.Path), 
+                provider));
+            main.Register("instructions", new GetInstructionsPathCommand(provider));
+            main.Register("verify", new VerifyCommand(
+                provider,
+                new JarFileVerifier(new JarFileProcessFactory())
+                ));
+
+            return main;
+        }
+    }
+}
