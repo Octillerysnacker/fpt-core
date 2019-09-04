e283a2c2232c31ea49b00518a12118d0d7666e80
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-03 21:15:00 -0500

    Changed PathProvider classes to only provide extension methods

--- FPT.Core/IO/LevelPathsProvider.cs
+++ FPT.Core/IO/LevelPathsProvider.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using FPT.Core.Levels;
+
 namespace FPT.Core.IO
 {
     public static class LevelPathsProvider
     {
-        public static string GetProjectFolder(string levelFolderPath,string user)
+        private static string GetProjectFolder(string levelFolderPath,string user)
         {
             return Path.Combine(GetUserFolder(levelFolderPath,user), "project");
         }
-        public static string GetUserFolder(string levelFolderPath, string user)
+        public static string GetProjectFolder(this Level level, string user)
+        {
+            return GetProjectFolder(level.FolderFilepath, user);
+        }
+        private static string GetUserFolder(string levelFolderPath, string user)
         {
             return Path.Combine(levelFolderPath, "users", user);
+        }
+        public static string GetUserFolder(this Level level, string user)
+        {
+            return GetUserFolder(level.FolderFilepath, user);
+        }
+        public static string GetInstructionsPath(this Level level)
+        {
+            return Path.Combine(level.FolderFilepath, level.InstructionsFilepath);
+        }
+        public static string GetVerifierFilepath(this Level level)
+        {
+            return Path.Combine(level.FolderFilepath, level.VerifierFilepath);
         }
     }
 }
--- FPT.Core/IO/MasterFolderPathsProvider.cs
+++ FPT.Core/IO/MasterFolderPathsProvider.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using FPT.Core.Levels;
+
 namespace FPT.Core.IO
 {
     public static class MasterFolderPathsProvider
     {
-        public static string GetMasterFolder(string levelFolderFilepath)
+        private static string GetMasterFolder(string levelFolderFilepath)
         {
             return Path.Combine(levelFolderFilepath, "master");
+        }
+        public static string GetMasterFolder(this Level level)
+        {
+            return GetMasterFolder(level.FolderFilepath);
         }
     }
 }
